0efc35f4b61013ea76baf7d619e117c9dce4c76c
Thomas Buchberger <t.buchberger@4teamwork.ch>
2017-01-06 15:23:28 +0100

    Initiate SAML2 protocol exchange in a IPubBeforeCommit handler instead of a viewlet

---
+++ ftw/saml2auth/subscribers.py
@@ -0,0 +1,98 @@
+from ZPublisher.interfaces import IPubBeforeCommit
+from base64 import b64encode
+from ftw.saml2auth.interfaces import IAuthNRequestStorage
+from ftw.saml2auth.interfaces import IServiceProviderSettings
+from ftw.saml2auth.utils import create_authn_request
+from plone.registry.interfaces import IRegistry
+from zope.component import adapter
+from zope.component import queryMultiAdapter
+from zope.component import queryUtility
+from zope.component.hooks import getSite
+
+
+@adapter(IPubBeforeCommit)
+def initiate_saml2_protocol_exchange(event):
+    site = getSite()
+    if site is None:
+        return
+
+    request = event.request
+    portal_state = queryMultiAdapter(
+        (site, request), name=u'plone_portal_state')
+
+    # Do not initiate if user is already authenticated.
+    if portal_state is None or not portal_state.anonymous():
+        return
+
+    registry = queryUtility(IRegistry)
+    settings = registry.forInterface(IServiceProviderSettings)
+
+    # Do not initiate if SAML2 authentication is disabled.
+    if not settings.enabled:
+        return
+
+    # Do not initiate if coming from IdP to prevent endless loop
+    if request.getHeader('HTTP_REFERER') == settings.idp_url:
+        return
+
+    current_url = request['ACTUAL_URL']
+    query_string = request['QUERY_STRING']
+    acs_url = u'{}/saml2/sp/sso'.format(
+        portal_state.portal_url().decode('utf8'))
+
+    # Do not initiate if calling our SAML2 endpoint
+    if current_url == acs_url:
+        return
+
+    # Allow logout and manual login
+    if (current_url.endswith('/logged_out') or
+            current_url.endswith('/login')):
+        return
+
+    # Create AuthNRequest
+    req = create_authn_request(
+        idp_url=settings.idp_url,
+        acs_url=acs_url,
+        issuer_id=settings.sp_issuer_id,
+        nameid_format=settings.nameid_format,
+        authn_context=settings.authn_context,
+        authn_context_comparison=settings.authn_context_comparison,
+        signing_key=settings.signing_key,
+        )
+
+    # Store id of AuthNRequest with current url.
+    issued_requests = IAuthNRequestStorage(site)
+    if query_string:
+        current_url = '{}?{}'.format(current_url, query_string)
+    issued_requests.add(req.ID, current_url)
+
+    # Replace current response with a form containing a SAML2 authentication
+    # request.
+    response = request.response
+    response.headers = {}
+    response.accumulated_headers = []
+    response.setBody(
+        ' '.join(FORM_TEMPLATE.format(
+            action=settings.idp_url,
+            authn_request=b64encode(req.toxml()),
+        ).split()))
+    response.setHeader('Expires', 'Sat, 01 Jan 2000 00:00:00 GMT')
+    response.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
+
+
+FORM_TEMPLATE = """
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+    </head>
+    <body onLoad="document.forms[0].submit();">
+        <form action="{action}" method="POST">
+            <input type="hidden" name="SAMLRequest" value="{authn_request}">
+            <span>If you are not automaticallly redirected click</span>
+            <input type="submit" value="Continue">
+        </form>
+    </body>
+</html>
+"""
